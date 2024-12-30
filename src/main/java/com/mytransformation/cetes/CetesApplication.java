256dd3093063cd9efb308653fa4bc38323f7e3a8
Carlos Herrera <juan.cervantes@rappi.com>
2024-12-30 17:18:04 -0600

    MT-38 Add base project

---
+++ src/main/java/com/mytransformation/cetes/CetesApplication.java
@@ -0,0 +1,13 @@
+package com.mytransformation.cetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytransformation/cetes/CetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytransformation.cetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
